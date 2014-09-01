67007af445caf657999d18781c0748209b4b6d67
Alli Dryer <awdryer@gmail.com>
2014-09-01 14:23:12 -0700

    Added animated logo, animated smiley faces.

--- Tipster/ViewController.swift
+++ Tipster/ViewController.swift
@@ -18,6 +18,14 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var tipControl: UISegmentedControl!
     
+    @IBOutlet weak var unhappy: UIImageView!
+    
+    @IBOutlet weak var happy: UIImageView!
+    
+    @IBOutlet weak var love: UIImageView!
+    
+    @IBOutlet weak var logo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -25,7 +33,47 @@ class ViewController: UIViewController {
         tipLabel.text = "$0.00"
         totalLabel.text = "$0.00"
         
+        let duration = 0.13
+        let delay = 0.2
+        let options = UIViewAnimationOptions.CurveEaseInOut
+        let damping = 0.5
+        let velocity = 1.0
+        
+        UIView.animateWithDuration(duration, delay: delay, options: options, animations: {
+            self.logo.transform = CGAffineTransformMakeRotation(0.5)
+        
+            }, completion: { finished in
+                UIView.animateWithDuration(duration, delay: 0.0, options: options, animations: {
+                    self.logo.transform = CGAffineTransformMakeRotation(0)
+                    }, completion: { finished in })
+        })
+        
+        self.unhappy.alpha = 0
+        self.happy.alpha = 0
+        self.love.alpha = 0
+        
         UISegmentedControl.appearance().setTitleTextAttributes(NSDictionary(objects: [UIFont(name: "Gotham Rounded", size: 18.0)], forKeys: [NSFontAttributeName]), forState: UIControlState.Normal)
+        
+        if tipControl.selectedSegmentIndex == 0 {
+            self.unhappy.alpha = 1
+        }
+        else {
+            self.unhappy.alpha = 0
+        }
+        
+        if tipControl.selectedSegmentIndex == 1 {
+            self.happy.alpha = 1
+        }
+        else {
+            self.happy.alpha = 0
+        }
+        
+        if tipControl.selectedSegmentIndex == 2 {
+            self.love.alpha = 1
+        }
+        else {
+            self.love.alpha = 0
+        }
         
     }
 
@@ -39,6 +87,61 @@ class ViewController: UIViewController {
         var tipPercentages = [0.18, 0.2, 0.22]
         var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
         
+        let duration = 0.13
+        let delay = 0.0
+        let options = UIViewAnimationOptions.CurveEaseInOut
+        let damping = 0.5
+        let velocity = 1.0
+        
+        if tipControl.selectedSegmentIndex == 0 {
+            
+            UIView.animateWithDuration(duration, delay: delay, options: options, animations: {
+                self.unhappy.alpha = 1
+                self.unhappy.frame = CGRect(x: self.unhappy.frame.origin.x, y: self.unhappy.frame.origin.y-8, width: self.unhappy.frame.width, height: self.unhappy.frame.height)
+                
+                }, completion: { finished in
+                    UIView.animateWithDuration(duration, delay: delay, options: options, animations: {
+                        self.unhappy.frame = CGRect(x: self.unhappy.frame.origin.x, y: self.unhappy.frame.origin.y+8, width: self.unhappy.frame.width, height: self.unhappy.frame.height)
+                    }, completion: { finished in })
+            })
+            
+        }
+        else {
+            self.unhappy.alpha = 0
+        }
+        
+        if tipControl.selectedSegmentIndex == 1 {
+            UIView.animateWithDuration(duration, delay: delay, options: options, animations: {
+                self.happy.alpha = 1
+                self.happy.frame = CGRect(x: self.happy.frame.origin.x, y: self.unhappy.frame.origin.y-8, width: self.happy.frame.width, height: self.happy.frame.height)
+                
+                }, completion: { finished in
+                    UIView.animateWithDuration(duration, delay: delay, options: options, animations: {
+                        self.happy.frame = CGRect(x: self.happy.frame.origin.x, y: self.happy.frame.origin.y+8, width: self.happy.frame.width, height: self.happy.frame.height)
+                        }, completion: { finished in })
+            })
+            
+        }
+        else {
+            self.happy.alpha = 0
+        }
+        
+        if tipControl.selectedSegmentIndex == 2 {
+            UIView.animateWithDuration(duration, delay: delay, options: options, animations: {
+                self.love.alpha = 1
+                self.love.frame = CGRect(x: self.love.frame.origin.x, y: self.love.frame.origin.y-8, width: self.love.frame.width, height: self.love.frame.height)
+                
+                }, completion: { finished in
+                    UIView.animateWithDuration(duration, delay: delay, options: options, animations: {
+                        self.love.frame = CGRect(x: self.love.frame.origin.x, y: self.love.frame.origin.y+8, width: self.love.frame.width, height: self.love.frame.height)
+                        }, completion: { finished in })
+            })
+            
+        }
+        else {
+            self.love.alpha = 0
+        }
+        
         var billAmount = billField.text._bridgeToObjectiveC().doubleValue
         var tip = billAmount * tipPercentage
         var total = billAmount + tip
@@ -48,10 +151,14 @@ class ViewController: UIViewController {
         
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
+        
+        
     }
 
     @IBAction func onTap(sender: AnyObject) {
         view.endEditing (true)
+        
     }
+
 }
 
